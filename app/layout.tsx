b7ba716fe76c3ba54bcb9ed737c512c735ce6f33
aydiiafrica <aydiiafrica@gmail.com>
2025-05-01 13:13:22 +0100

    First Commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title:
+    'Aydiiafrica - Advocacy for youth women and girls development impact initiative for Africa (AYDii Africa)',
+  description:
+    'A non-governmental organization dedication to the synergy of organizations, communities, and relevant stakeholders to create innovative, impactful, and sustainable solutions in promoting climate action, addressing critical socio-economic challenges and ensuring peace justice and strong institution.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="h-svh w-full flex items-center justify-center">
+      <figure className="w-[20rem] h-[20rem] overflow-hidden">
+        <img
+          src="/logo.png"
+          alt="Aydiiafrica Logo"
+          className="w-full h-full object-cover"
+        />
+      </figure>
+    </div>
+  );
+}
